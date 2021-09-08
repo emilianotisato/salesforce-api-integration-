150f68d9887763ac17ceddad53b5088ea7960b75
Emiliano Tisato <emilianotisato@gmail.com>
2021-09-08 06:31:57 -0500

    Test fetching contacts from salesforce

--- app/Services/SalesforceApi.php
+++ app/Services/SalesforceApi.php
@@ -3,12 +3,14 @@
 namespace App\Services;
 
 use Exception;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Cache;
 
 class SalesforceApi
 {
     public $http;
+    public string $module;
     public string $token;
 
     public function __construct()
@@ -32,4 +34,25 @@ class SalesforceApi
             'base_uri' => config('salesforce.base_api_url'),
         ])->retry(5, 2000);
     }
+
+    /**
+     * Sets access to contacts module
+     *
+     * @return self
+     */
+    public function contacts() : self
+    {
+        $this->module = 'contacts';
+
+        return $this;
+    }
+
+    public function all() : Collection
+    {
+       return collect(
+           json_decode($this->http->get($this->module.'/')->body())->records
+       );
+
+    }
 }
+
--- tests/Unit/Services/SalesforceApiTest.php
+++ tests/Unit/Services/SalesforceApiTest.php
@@ -4,13 +4,14 @@ namespace Tests\Unit\Services;
 
 use Tests\TestCase;
 use App\Services\SalesforceApi;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Cache;
 
 class SalesforceApiTest extends TestCase
 {
     /** @test */
-    public function it_obteins_the_auth_token_from_cache_or_renew_it()
+    public function it_obteins_the_auth_token_from_cache_or_renew_it_if_missing()
     {
         $this->app['config']->set('salesforce.token_ttl', 600); // 10 minutes
 
@@ -19,7 +20,7 @@ class SalesforceApiTest extends TestCase
             ->with('salesforce_token')
             ->andReturn(null);
 
-        Http::fake([config('salesforce.base_api_url') . 'login/' => Http::response([
+        Http::fake([config('salesforce.base_api_url') . 'login/' => http::response([
             "message" => "Success",
             "token" => "some-token"
         ])]);
@@ -29,5 +30,36 @@ class SalesforceApiTest extends TestCase
             ->with('salesforce_token', 'some-token', 600);
 
         new SalesforceApi();
+    }
+
+    /** @test */
+    public function it_fetch_a_list_of_contacts_from_salesforce()
+    {
+        Cache::put('salesforce_token', 'some-token', config('salesforce.token_ttl'));
+
+        Http::fake([config('salesforce.base_api_url') . 'contacts/' => Http::response([
+            "records" => [
+                [
+                    "account_id" => "0015e00000F2CgJAAV",
+                    "description" => null,
+                    "email" => "agreen@uog.com",
+                    "first_name" => "Avi",
+                    "id" => "0035e00000BVJ4hAAH",
+                    "is_deleted" => false,
+                    "last_name" => "Green",
+                    "lead_source" => "Public Relations",
+                    "title" => "CFO"
+                ]
+            ],
+            "status" => true,
+            "total" => 1
+        ])]);
+
+        $salesforce = new SalesforceApi();
+        $contacts = $salesforce->contacts()->all();
+
+        $this->assertInstanceOf(Collection::class, $contacts);
+        $this->assertEquals(1, $contacts->count());
+        $this->assertEquals('0035e00000BVJ4hAAH', $contacts->first()->id);
     }
 }
